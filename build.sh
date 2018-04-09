2aa969ca2466ef8db51eb92e67be490bdd6ae2fd
mwbehr1 <33470624+mwbehr1@users.noreply.github.com>
2018-04-09 18:52:10 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm --privileged \
-    -v ~/.docker:/root/.docker \
-    homeassistant/amd64-builder \
-    --all \
-    -r https://github.com/mwbehr1/bluetooth-bcm43xx-3bpl.git \
-    -b master \
-    -t Bluetooth-BCM43xx-3Bp
